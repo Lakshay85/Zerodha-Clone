b3d66ac7c8dea731785660d3b28ded05c7344f0d
Lakshay Rohilla <rohillalakshay85@gmail.com>
2025-02-11 21:43:55 +0530

    working on backend

--- backend/model/UsersModel.js
+++
@@ -1,7 +0,0 @@
-const {model} = require('mongoose');
-
-const {UsersSchema} = require('../schemas/UsersSchema');
-
-const UsersModel = new model("users",UsersSchema);
-
-module.exports = {UsersModel};
